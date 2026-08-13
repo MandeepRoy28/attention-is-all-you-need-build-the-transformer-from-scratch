83b13bd4fbcb8e3b63a5c8c2cec748a67f91a3f1
MandeepRoy28 <98833294+MandeepRoy28@users.noreply.github.com>
2026-08-13 11:05:05 +0900

    Step 56: shift_targets_right_with_start_token

--- model.py
+++ model.py
@@ -700,8 +700,13 @@ def collect_model_parameters_into_list(encoder_layer_params, decoder_layer_param
 
     return param_list
 
-# Step 56 - shift_targets_right_with_start_token (not yet solved)
-# TODO: implement
+# Step 56 - shift_targets_right_with_start_token
+def shift_targets_right_with_start_token(target_ids, start_token_id):
+    # TODO: prepend start_token_id and drop the last column so output shape matches target_ids
+    shift = target_ids.clone()
+    shift[:,0] = start_token_id
+    shift[:,1:] = target_ids[:,0:-1]
+    return shift
 
 # Step 57 - compute_noam_learning_rate (not yet solved)
 # TODO: implement
